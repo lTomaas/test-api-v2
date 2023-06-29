46bfca0ca13821a185f6cc387a3a94bfa6aed2c1
lTomaas <psilva@fi.uba.ar>
2023-06-29 05:09:53 -0300

    test

--- views/scripts/librosTabla.js
+++ views/scripts/librosTabla.js
@@ -2,11 +2,8 @@
 const dominio = `https://${window.location.host}/api/libros`
 
 
-//document.write(dominio);
-
 fetch(dominio)
     .then(function (response) {
-        //document.write("fetch");
         return response.json();
     })
 
@@ -23,6 +20,7 @@ fetch(dominio)
                     <td>${libro.nombre} </td>
                     <td>${libro.autor} </td>
                     <td>${libro.precio} </td>
+                    <td><img src="${libro.url_imagen}.jpg" alt="imagen" width = "100px"></td>
                 </tr>
                 `
             //document.write(out);
